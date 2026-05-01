301ede0991b34b6080911b4583274ac303a4ffab
fredjrp <110814718+fredjrp@users.noreply.github.com>
2026-05-01 13:56:40 +0300

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,24 @@
 import 'package:flutter/material.dart';
+import 'onboarding_screen.dart';
+import 'app.dart'; // Change this to your actual file name that contains SampleNavigationApp
 
-import 'app.dart';
+void main() => runApp(const MyApp());
 
-void main() => runApp(const SampleNavigationApp());
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Navigation App',
+      theme: ThemeData(
+        fontFamily: 'Mulish', // Using your onboarding font
+      ),
+      home: const OnboardingScreen(),
+      routes: {
+        '/navigation': (context) => const SampleNavigationApp(),
+      },
+    );
+  }
+}
